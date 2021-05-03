4351302378dba157a08821787adec528cacaab54
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-03 10:58:54 +0200

    exercise num 4 solved

---
+++ 06.dom/04.attr-create/script.js
@@ -0,0 +1,22 @@
+/* becode/javascript
+ *
+ * /06-dom/04-attr-create/script.js - 6.4: manipulation d'attributs & création d'élément
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    const span = document.getElementById("source");
+    const img = document.createElement("img");
+    img.src = span.getAttribute('data-image');
+
+    const figure = document.getElementById("target");
+    figure.appendChild(img);
+
+})();
