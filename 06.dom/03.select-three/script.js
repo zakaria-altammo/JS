0ab4bd34de829441774093f13378e580cca195e7
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-03 10:15:40 +0200

    exercise num 3 solved

---
+++ 06.dom/03.select-three/script.js
@@ -0,0 +1,18 @@
+/* becode/javascript
+ *
+ * /06-dom/03-select-three/script.js - 6.3: sélection multiple par sélecteur css
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    let target = document.querySelectorAll(".target");
+    target.forEach(element => element.innerHTML = "owned");
+
+})();
