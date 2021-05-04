20fd73ac90af3ed8e4576a4ed8f26f262b047402
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-04 16:48:13 +0200

    exercise num 1 i hope its correct

---
+++ 08.fetch/01.list-to-console/script.js
@@ -0,0 +1,20 @@
+/* becode/javascript
+ *
+ * /09-fetch/01-list-to-console/script.js - 11.1: liste vers console
+ *
+ * coded by leny@BeCode
+ * started at 12/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+    document.getElementById("run").addEventListener("click", heroes)
+        async function heroes(){
+            let data = await fetch('../../_shared/api.json')
+            let main = await data.json();
+            console.log(main);
+        }
+})();
