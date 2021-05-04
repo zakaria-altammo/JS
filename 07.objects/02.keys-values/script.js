45a1ff49e5d770079c399a1e4b91949e45299a19
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-04 14:21:07 +0200

    exercise num 2 of objects

---
+++ 07.objects/02.keys-values/script.js
@@ -0,0 +1,26 @@
+/* becode/javascript
+ *
+ * /06-objects/03-keys-values/script.js - 6.3: clés & valeurs
+ *
+ * coded by leny@BeCode
+ * started at 08/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    document.getElementById("run").addEventListener("click", E => {
+        // your code here
+        const person = {
+            lastname: "Delnatte",
+            firstname: "Pierre-Antoine",
+            nickname: "Leny",
+            birthDate: "08-05-1985",
+            city: "Liège",
+            status: "married",
+        };
+        console.log(Object.keys(person));
+        console.log(Object.values(person));
+    });
+})();
